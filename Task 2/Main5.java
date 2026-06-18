0a9173f6e56ddd4de69db76121b192f275cf6cdb
tanmoyp123 <tanmoyp129@gmail.com>
2026-06-18 16:42:47 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,17 @@
+class Printer{
+    void printData(string text){
+        System.out.println("text =" +text);
+    }
+    void printData(int number){
+        System.out.println("number =" +number);
+    }
+}    
+    public class Main {
+        public static void main(String[] args) {
+             Printer printer = new Printer();
+
+        printer.printData("Hello Printer");
+        printer.printData(100);
+        }
+    }
 
