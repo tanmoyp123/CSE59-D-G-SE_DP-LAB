a52538cd02dc06d0b6d277e190871c2803635398
tanmoyp123 <tanmoyp129@gmail.com>
2026-06-18 16:55:33 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,18 @@
 
+interface Clickable {
+    void onClick();
+}
+
+class Button implements Clickable {
+
+   
+    public void onClick() {
+        System.out.println("Button was clicked!");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Button button = new Button();
+        button.onClick();
+    }
+}
