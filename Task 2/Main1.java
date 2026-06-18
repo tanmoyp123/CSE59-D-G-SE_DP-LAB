e78cb3dd0e5f92a923aaae6d4a7d900ecdc4542f
tanmoyp123 <tanmoyp129@gmail.com>
2026-06-18 16:08:17 +0600

    Add ContestStats class to manage contest scores

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,19 @@
+class ContestStats {
+    
+    int[] scores = {85, 90, 95};
 
+    
+    void printFirstScore() {
+        System.out.println("First Score: " + scores[0]);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+   
+        ContestStats contest = new ContestStats();
+
+
+        contest.printFirstScore();
+    }
+}
