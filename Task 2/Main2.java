49727dd44a474a81ab2f6369f3c15d6bb026d03b
tanmoyp123 <tanmoyp129@gmail.com>
2026-06-18 16:19:11 +0600

    Update Main2.java

--- Task 2/Main2.java
+++ Task 2/Main2.java
@@ -1,1 +1,17 @@
-
+class NumberCheacker{
+    void check(int num) {
+         boolean result = (Num %2 == 0) && (Num > 10);
+           System.out.println("result");
+           
+        
+           
+                 
+            
+    }
+}
+public class main {
+    public static void main(String[] args) {
+         NumberChecker checker = new NumberChecker();
+         checker.check(14);
+    }
+}
